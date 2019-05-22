8708524bbfe9ec6ad99ec20ea0ce86e11f9a2314
Tedu <3412836957@qq.com>
2019-05-22 14:32:11 +0800

    change week01

--- week01.py
+++ week01.py
@@ -1,23 +1,4 @@
-# 用while循环写代码，求1/1+1/3+1/5+．．．．+1/99的值
-# sum = 0
-# denominator = 1
-# while denominator <= 99:
-#     print("while后面的分母是:", denominator)
-#     sum += 1/denominator
-#     denominator += 2
-# print(sum)
+import sys
 
-
-d = {1:'a', 2:'b', 3:'4'}
-
-print(d[2])
-
-
-
-
-
-
-
-
-
-
+# 获取命令行
+print(sys.exit())
